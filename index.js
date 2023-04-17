d86e6f7e50d099a088a29a2a85c8252407b2b163
Alisa <alisakovalhome@mail.ru>
2023-04-17 15:17:25 +0300

    Add solutions

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 // Код для первого задания напиши в этой функции
-function crawlTree() {}
+
+function crawlTree(arr) {
+
+    if (!Array.isArray(arr)) {
+        return arr
+    }
+    else {
+        if (arr.length > 1) {
+            return crawlTree(arr[0]) + crawlTree(arr.slice(1))
+        } else {
+            return crawlTree(arr[0])
+        }
+    }
+}
+
+console.log(crawlTree([
+    [
+        [["П", "р"], "и"],
+        ["в", ["е", "т"]],
+    ],
+    [["и", "к"], "и"],
+]));
 
 // Код для второго задания напиши в этой функции
-function twoSum() {}
+function twoSum(arr, target) {
+    function executer(i, j) {
+        if (arr[i] + arr[j] == target) {
+            return [i, j]
+        }
+        if (j < arr.length) {
+            return executer(i, j + 1)
+        } else {
+            if (i < arr.length - 1) {
+                return executer(i + 1, i + 2)
+            }
+        }
+    }
+    return executer(0, 1)
+}
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
